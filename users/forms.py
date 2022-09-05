127bf98c159aa0d9e04bca1a884b8a59ea76f919
campbell-lucas <campbell_lucas@outlook.com>
2022-09-05 20:10:36 +0200

    update user model, add registration view

---
+++ users/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+
+
+class RegistrationForm(forms.ModelForm):
+    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name', 'email', 'is_sales_manager', 'is_collector', 'password')
+
+    def clean_password_confirmation(self):
+        password = self.cleaned_data.get('password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+
+        if password and password_confirmation and password != password_confirmation:
+            raise ValidationError('Passwords don`t match')
+
+        return password_confirmation
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+
+        if commit:
+            user.save()
+
+        return user
