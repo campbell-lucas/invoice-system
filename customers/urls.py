39aec6ab46272b21350af0fb7f6199e63856a989
campbell-lucas <campbell_lucas@outlook.com>
2022-09-23 17:21:44 +0200

    edited customers invoice view html file, urls and views file

--- customers/urls.py
+++ customers/urls.py
@@ -6,7 +6,7 @@ app_name = 'customers'
 
 urlpatterns = [
     path('create/', views.CreateCustomerView.as_view(), name='create-customer'),
-    path('customers/', views.CustomerView.as_view(), name='customer_list'),
+    # path('customers/', views.CustomerView.as_view(), name='customer_list'),
     path('customers/<slug:slug>', views.CustomerDetailView.as_view(), name='customer_detail_view'),
     path('customers/<slug:slug>/invoices/<int:pk>/', views.CustomerInvoicesView.as_view(), name='customer_invoice_view'),
 
--- customers/views.py
+++ customers/views.py
@@ -6,13 +6,20 @@ from users.models import CustomUser
 from . import models
 
 
-class CustomerView(LoginRequiredMixin, ListView):
-    model = models.Customer
-    template_name = 'customers/customer_list_view.html'
-    context_object_name = 'customers'
+# class CustomerView(LoginRequiredMixin, ListView):
+#     """
+#     Class CustomerView uses ListView to show a list of all customers under the specific user.
+#     """
+#     model = models.Customer
+#     template_name = 'customers/customer_list_view.html'
+#     context_object_name = 'customers'
 
 
 class CreateCustomerView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    """
+    Class CreateCustomerView is for creating a new customer and uses LoginRequiredMixin and
+    PermissionRequiredMixin to make sure that only users in group sales manager can access it.
+    """
     model = models.Customer
     fields = [
         'number',
@@ -37,25 +44,40 @@ class CreateCustomerView(LoginRequiredMixin, PermissionRequiredMixin, CreateView
     success_url = reverse_lazy('home:home')
 
     def get_form(self, *args, **kwargs):
+        """
+        Gets the form / information that was input into the form shown.
+        """
         form = super(CreateCustomerView, self).get_form(*args, **kwargs)
         form.fields['collector'].queryset = CustomUser.objects.all().filter(is_collector=True)
         form.fields['sales_manager'].queryset = CustomUser.objects.all().filter(is_sales_manager=True)
         return form
 
     def form_valid(self, form):
+        """
+        makes the instance owner of the form the requester, and then returns the super of get_form
+        """
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
 
 class CustomerDetailView(LoginRequiredMixin, DetailView):
+    """
+    This class is based on model Customer, and it used to show the details of the customer i.e. Email, Phone Number
+    """
     model = models.Customer
     template_name = 'customers/customer_detailed_view.html'
 
 
 class CustomerInvoicesView(LoginRequiredMixin, ListView):
+    """
+    This class is based on the model Invoice, and uses ListView to list all the customers invoices individually
+    """
     model = Invoice
     template_name = 'customers/customer_invoice_view.html'
     context_object_name = 'invoices'
 
     def get_queryset(self):
+        """
+        This returns the specific customers queryset to show only that customers invoices.
+        """
         return Invoice.objects.filter(customer_id=self.kwargs.get('pk'))
